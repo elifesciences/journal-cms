5880587a0c8bddbf919dcd90427c38e7b88c2743
nlisgo <nathan@aeopus.com>
2016-05-26 13:58:54 +0100

    Upgrade to the latest version of Drupal Console

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -18,6 +18,7 @@ class ScriptHandler extends DrupalScriptHandler {
     $root = getcwd();
     $drupal_root = static::getDrupalRoot($root);
     $config_root = $root . '/config';
+    $src_root = $root . '/src';
     parent::createRequiredFiles($event);
     if ($fs->exists($config_root . '/settings.php')) {
       if ($fs->exists($drupal_root . '/sites/default/settings.php')) {
@@ -25,7 +26,7 @@ class ScriptHandler extends DrupalScriptHandler {
         $fs->remove($drupal_root . '/sites/default/settings.php');
       }
       $fs->copy($config_root . '/settings.php', $drupal_root . '/sites/default/settings.php');
-      $fs->chmod($drupal_root . '/sites/default/settings.php', 0666);
+      $fs->chmod($drupal_root . '/sites/default/settings.php', 0755);
       $event->getIO()->write('Create a sites/default/settings.php file from local config with chmod 0666');
     }
 
@@ -34,7 +35,7 @@ class ScriptHandler extends DrupalScriptHandler {
     }
 
     // Create symlink to custom modules folder.
-    if ($fs->exists('./../src/modules') && !$fs->exists($drupal_root . '/modules/custom')) {
+    if ($fs->exists($src_root . '/modules') && !$fs->exists($drupal_root . '/modules/custom')) {
       $fs->symlink('../../src/modules', $drupal_root . '/modules/custom');
     }
 
