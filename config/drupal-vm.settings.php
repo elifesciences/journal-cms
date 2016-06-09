f42fd934ef92ac3b401ee491ea02742df0b97b4b
nlisgo <nathan@aeopus.com>
2016-06-09 11:15:06 +0100

    Fix vm db credentials

--- config/drupal-vm.settings.php
+++ config/drupal-vm.settings.php
@@ -1,26 +1,25 @@
 <?php
 
-$databases['default'] = [
-  'default' => [
-    'database' => 'elife_2_0',
-    'username' => 'elife_2_0',
-    'password' => 'elife_2_0',
-    'prefix' => '',
-    'host' => 'localhost',
-    'port' => '3306',
-    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-    'driver' => 'mysql',
-  ],
-  'elife_1_0' => [
-    'database' => 'elife_1_0',
-    'username' => 'elife_1_0',
-    'password' => 'elife_1_0',
-    'prefix' => '',
-    'host' => 'localhost',
-    'port' => '3306',
-    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-    'driver' => 'mysql',
-  ],
+$databases['default']['default'] = [
+  'database' => 'elife_2_0',
+  'username' => 'elife_2_0',
+  'password' => 'elife_2_0',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+
+$databases['elife_1_0']['default'] = [
+  'database' => 'elife_1_0',
+  'username' => 'elife_1_0',
+  'password' => 'elife_1_0',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
 ];
 
 $settings['trusted_host_patterns'] = [
