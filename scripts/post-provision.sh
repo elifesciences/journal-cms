f9ab063e79e2e6bf4ce0140ab10524f255ad64e4
nlisgo <nathan@aeopus.com>
2016-06-09 11:14:46 +0100

    Set default directory when connecting to virtual machine

--- scripts/post-provision.sh
+++ scripts/post-provision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# If a source database is available, import it.
 ELIFE_1_0_IMPORTED=/etc/elife_1_0_imported.cnf
 
 if [[ -e /vagrant/scripts/elife_1_0.sql.gz && ! -e "$ELIFE_1_0_IMPORTED" ]]; then
@@ -9,6 +10,11 @@ if [[ -e /vagrant/scripts/elife_1_0.sql.gz && ! -e "$ELIFE_1_0_IMPORTED" ]]; the
     echo "host = localhost" | sudo tee -a $ELIFE_1_0_IMPORTED
 
     zcat /vagrant/scripts/elife_1_0.sql.gz | mysql --defaults-extra-file=$ELIFE_1_0_IMPORTED elife_1_0
-else
-    exit 0
 fi
+
+# Set the default directory when accessing the vm.
+if [[ ! -e /home/vagrant/.bashrc ]]; then
+    echo "cd /var/www/elife-2.0-website" >> /home/vagrant/.bashrc
+fi
+
+exit 0
