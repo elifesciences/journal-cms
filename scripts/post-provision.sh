67b04f2052fb243f53222b03e84b7534025d90e8
nlisgo <nathan@aeopus.com>
2016-06-09 12:02:25 +0100

    Add vendor/bin to PATH

--- scripts/post-provision.sh
+++ scripts/post-provision.sh
@@ -12,9 +12,10 @@ if [[ -e /vagrant/scripts/elife_1_0.sql.gz && ! -e "$ELIFE_1_0_IMPORTED" ]]; the
     zcat /vagrant/scripts/elife_1_0.sql.gz | mysql --defaults-extra-file=$ELIFE_1_0_IMPORTED elife_1_0
 fi
 
-# Set the default directory when accessing the vm.
+# Set the default directory when accessing the vm and add vendor bin to PATH.
 if [[ ! -e /home/vagrant/.bashrc ]]; then
     echo "cd /var/www/elife-2.0-website" >> /home/vagrant/.bashrc
+    echo "export PATH=\"/var/www/elife-2.0-website/vendor/bin:$PATH\"" >> /home/vagrant/.bashrc
 fi
 
 exit 0
