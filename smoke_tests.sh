40279c4b55e185a49e5d8092d6d3449772ba840d
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-11 16:42:23 +0100

    Adding CI step for project tests
    Only smoke tests are run.

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Host: elife-2.0-website.dev" localhost) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null localhost) == 200 ]
