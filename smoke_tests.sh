644275b72020045b6ce2b9799f18912cd20b0e26
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-25 09:40:33 +0100

    Updated smoke tests to use correct hostname

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null localhost) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)) == 200 ]
