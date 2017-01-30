c0ae949a6ae1fa33ecad0f4583bc6b86d83aae07
Giorgio Sironi <g.sironi@elifesciences.org>
2017-01-30 15:30:52 +0000

    Performing smoke tests on all APIs on deploy

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -2,3 +2,8 @@
 set -ex
 
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)) == 200 ]
+for api in labs-experiment subject podcast-episode blog-article event interview collection cover; do
+    [ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.$api-list+json; version=1" $(hostname)/"${api}s") == 200 ]
+done
+
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.person-list+json; version=1" $(hostname)/people) == 200 ]
