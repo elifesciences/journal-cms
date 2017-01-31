d817f7816582f44f5f5cb90a96a51e6d4a3c2ac3
Giorgio Sironi <g.sironi@elifesciences.org>
2017-01-31 09:27:09 +0000

    Unrolled loop of requests

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -4,9 +4,16 @@ set -ex
 local_hostname=$(hostname)
 hostname=${1:-$local_hostname}
 
+echo "Homepage"
 [ $(curl --write-out %{http_code} --silent --output /dev/null "$hostname") == 200 ]
-for api in labs-experiment subject podcast-episode blog-article event interview collection cover; do
-    [ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.$api-list+json; version=1" "$hostname"/"${api}s") == 200 ]
-done
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.person-list+json; version=1" "$hostname"/people) == 200 ]
+echo "APIs"
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.blog-article-list+json; version=1" "${hostname}/blog-articles") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.collection-list+json; version=1" "${hostname}/collections") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.cover-list+json; version=1" "${hostname}/covers") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.event-list+json; version=1" "$hostname"/"${hostname}/events") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.interview-list+json; version=1" "${hostname}/interviews") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.labs-experiment-list+json; version=1" "${hostname}/labs-experiments") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.person-list+json; version=1" "${hostname}/people") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.podcast-episode-list+json; version=1" "${hostname}/podcast-episodes") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.subject-list+json; version=1" "${hostname}/subjects") == 200 ]
