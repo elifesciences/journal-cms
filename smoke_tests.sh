07330515480d8dae9678a21ee032842f424e14be
Giorgio Sironi <g.sironi@elifesciences.org>
2017-01-31 09:07:30 +0000

    Hostname can be passed as first argument
    ```
    ./smoke_tests.sh localhost
    ```

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -ex
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)) == 200 ]
+local_hostname=$(hostname)
+hostname=${1:-$local_hostname}
+
+[ $(curl --write-out %{http_code} --silent --output /dev/null "$hostname") == 200 ]
 for api in labs-experiment subject podcast-episode blog-article event interview collection cover; do
-    [ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.$api-list+json; version=1" $(hostname)/"${api}s") == 200 ]
+    [ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.$api-list+json; version=1" "$hostname"/"${api}s") == 200 ]
 done
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.person-list+json; version=1" $(hostname)/people) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null -H "Accept: application/vnd.elife.person-list+json; version=1" "$hostname"/people) == 200 ]
