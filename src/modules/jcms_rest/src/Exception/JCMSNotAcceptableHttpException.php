07479f25fd4087d5cede7c160f23997ce94fca25
Jamie Hollern <jamiehollern@gmail.com>
2017-02-01 17:05:07 +0000

    Fixed the case of the JCMS Rest exception files and class names.

--- src/modules/jcms_rest/src/Exception/JCMSNotAcceptableHttpException.php
+++ src/modules/jcms_rest/src/Exception/JCMSNotAcceptableHttpException.php
@@ -6,15 +6,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
- * This should be used instead of the Symfony NotFoundHttpException as we
+ * This should be used instead of the Symfony NotAcceptableHttpException as we
  * need to set the content type header.
  *
  * @package Drupal\jcms_rest\Exception
  */
-class JcmsNotFoundHttpException extends HttpException {
+class JCMSNotAcceptableHttpException extends HttpException {
 
   public function __construct($message, \Exception $previous = NULL, $media_type, $code = 0) {
-    parent::__construct(Response::HTTP_NOT_FOUND, $message, $previous, ['Content-Type' => $media_type], $code);
+    parent::__construct(Response::HTTP_NOT_ACCEPTABLE, $message, $previous, ['Content-Type' => $media_type], $code);
   }
 
 }
