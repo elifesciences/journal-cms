3d49730d261e9092816d0ed6bc9497f82e3d3745
Jamie Hollern <jamiehollern@gmail.com>
2017-02-16 11:53:39 +0000

    Fix Travis MySQL issue.
    Debug.
    
    Debug.
    
    Bugfix.
    
    Fix.

--- scripts/travis/mysql-reset-root-password.sh
+++ scripts/travis/mysql-reset-root-password.sh
@@ -2,10 +2,8 @@
 set -x
 set -e
 sudo service mysql stop
-sudo  mysqld_safe --skip-grant-tables &
+sudo  mysqld_safe --skip-grant-tables --pid-file=/var/lib/mysql/mysqld_safe.pid &
 sleep 4
 sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
 sudo kill -9 `sudo cat /var/lib/mysql/mysqld_safe.pid`
-sudo kill -9 `sudo cat /var/run/mysqld/mysqld.pid`
-sudo service mysql restart
 sleep 4
