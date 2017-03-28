e2a69592de6dd020d8df741de62c7a02e7ba75a4
Nathan Lisgo <nathan@aeopus.com>
2017-03-28 15:06:49 +0100

    Add ping resource and test
    * Add ping resource and test
    * Adding charset to /ping
    * Set cache-control explicitly

---
+++ src/modules/jcms_ping/src/Controller/PingController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\jcms_ping\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Controller to handle a ping request for journal-cms.
+ */
+class PingController {
+
+  /**
+   * String sent in responses, to verify site status.
+   *
+   * @var string
+   */
+  const SITE_STATUS_RESPONSE = 'pong';
+
+  /**
+   * Checks the site status.
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
+   *   The response.
+   */
+  public function siteStatus() {
+    $response = new Response(self::SITE_STATUS_RESPONSE);
+    $response->headers->set('Content-Type', 'text/plain; charset=UTF-8');
+    $response->headers->set('Cache-Control', 'must-revalidate, no-cache, no-store, private');
+    return $response;
+  }
+
+}
