9b9cb758b2680d9d545920cd27437bf054ef80ac
Nathan Lisgo <nathan@aeopus.com>
2017-07-24 13:22:55 +0100

    Fix issue with empty affiliation names and getVenue code formatting (#229)

--- src/modules/jcms_rest/src/Plugin/rest/resource/AbstractRestResourceBase.php
+++ src/modules/jcms_rest/src/Plugin/rest/resource/AbstractRestResourceBase.php
@@ -508,55 +508,55 @@ abstract class AbstractRestResourceBase extends ResourceBase {
    * @return array
    */
   protected function getVenue(Paragraph $venue_field) {
-      $venue = [
-        'name' => array_values(array_filter(preg_split("(\r\n?|\n)", $venue_field->get('field_block_title_multiline')->getString()))),
+    $venue = [
+      'name' => array_values(array_filter(preg_split("(\r\n?|\n)", $venue_field->get('field_block_title_multiline')->getString()))),
+    ];
+
+    // Venue address is optional.
+    if ($venue_field->get('field_block_address')->count()) {
+      $locale = 'en';
+      /* @var \CommerceGuys\Addressing\AddressInterface $address  */
+      $address = $venue_field->get('field_block_address')->first();
+      $postal_label_formatter = \Drupal::service('address.postal_label_formatter');
+      $postal_label_formatter->setOriginCountryCode('no_origin');
+      $postal_label_formatter->setLocale($locale);
+      $components = [
+        'streetAddress' => ['getAddressLine1', 'getAddressLine2'],
+        'locality' => ['getLocality', 'getDependentLocality'],
+        'area' => ['getAdministrativeArea'],
       ];
 
-      // Venue address is optional.
-      if ($venue_field->get('field_block_address')->count()) {
-        $locale = 'en';
-        /* @var \CommerceGuys\Addressing\AddressInterface $address  */
-        $address = $venue_field->get('field_block_address')->first();
-        $postal_label_formatter = \Drupal::service('address.postal_label_formatter');
-        $postal_label_formatter->setOriginCountryCode('no_origin');
-        $postal_label_formatter->setLocale($locale);
-        $components = [
-          'streetAddress' => ['getAddressLine1', 'getAddressLine2'],
-          'locality' => ['getLocality', 'getDependentLocality'],
-          'area' => ['getAdministrativeArea'],
-        ];
+      $venue['address'] = [
+        'formatted' => explode("\n", $postal_label_formatter->format($address)),
+        'components' => [],
+      ];
 
-        $venue['address'] = [
-          'formatted' => explode("\n", $postal_label_formatter->format($address)),
-          'components' => [],
-        ];
-
-        foreach ($components as $section => $methods) {
-          $values = [];
-          foreach ($methods as $method) {
-            if ($value = $address->{$method}()) {
-              $values[] = $value;
-            }
-          }
-
-          if (!empty($values)) {
-            $venue['address']['components'][$section] = $values;
+      foreach ($components as $section => $methods) {
+        $values = [];
+        foreach ($methods as $method) {
+          if ($value = $address->{$method}()) {
+            $values[] = $value;
           }
         }
 
-        $country_repository = \Drupal::service('address.country_repository');
-        $countries = $country_repository->getList($locale);
-        $venue['address']['components']['country'] = $countries[$address->getCountryCode()];
-
-        if ($postal_code = $address->getPostalCode()) {
-          $venue['address']['components']['postalCode'] = $postal_code;
-        }
-        elseif ($postal_code = $address->getSortingCode()) {
-          $venue['address']['components']['postalCode'] = $postal_code;
+        if (!empty($values)) {
+          $venue['address']['components'][$section] = $values;
         }
       }
 
-      return $venue;
+      $country_repository = \Drupal::service('address.country_repository');
+      $countries = $country_repository->getList($locale);
+      $venue['address']['components']['country'] = $countries[$address->getCountryCode()];
+
+      if ($postal_code = $address->getPostalCode()) {
+        $venue['address']['components']['postalCode'] = $postal_code;
+      }
+      elseif ($postal_code = $address->getSortingCode()) {
+        $venue['address']['components']['postalCode'] = $postal_code;
+      }
+    }
+
+    return $venue;
   }
 
   /**
