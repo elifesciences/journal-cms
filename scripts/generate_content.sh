902f18a791fd5ec0a39d3317a691b00d13173855
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-18 13:50:17 +0000

    Generating many different content types

--- scripts/generate_content.sh
+++ scripts/generate_content.sh
@@ -8,7 +8,9 @@ echo "Creating content type subjects"
 ../vendor/bin/drush --nocolor generate-terms subjects 5 --kill
 
 # labs_experiment is actually published as /labs-posts
-for type in blog_article labs_experiment person event; do
+for type in blog_article labs_experiment person event podcast_episode interview collection cover press_package annual_report; do
     echo "Creating content type $type"
     ../vendor/bin/drush --nocolor generate-content 5 --types=$type
 done
+# community is missing
+# highlights is missing
