905130e7470dc513005749e37c4712b6d3db39d4
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-18 13:39:55 +0000

    Correctly creating all 3 content types

--- scripts/generate_content.sh
+++ scripts/generate_content.sh
@@ -2,7 +2,8 @@
 set -e
 
 cd ./web
-../vendor/bin/drush -y en devel_generate
-../vendor/bin/drush generate-terms 5 subjects --kill
-../vendor/bin/drush generate-content 5 --types='labs_experiment'
-../vendor/bin/drush generate-content 5 --types='event'
+../vendor/bin/drush --nocolor -y en devel_generate
+../vendor/bin/drush --nocolor generate-terms subjects 5 --kill
+# this is actually published as /labs-posts
+../vendor/bin/drush --nocolor generate-content 5 --types='labs_experiment'
+../vendor/bin/drush --nocolor generate-content 5 --types='event'
