bc99b47fe4b60202739c39b19af9678da437136e
nlisgo <nathan@aeopus.com>
2017-09-21 13:46:00 +0100

    Improve random orcid generation

--- src/modules/jcms_rest/src/Plugin/Field/FieldTypeExtend/StringItem.php
+++ src/modules/jcms_rest/src/Plugin/Field/FieldTypeExtend/StringItem.php
@@ -17,12 +17,14 @@ class StringItem extends StringItemExtend {
     switch ($field_definition->getName()) {
       // Random values for orcid need to match the regex ^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{3}[0-9X]$.
       case 'field_person_orcid':
-        $orcid = [4, 4, 4, 3];
-        array_walk($orcid, function (&$value) {
-          $value = str_pad(rand(0, pow(10, $value-1)-1), $value, '0', STR_PAD_LEFT);
-        });
+        // Generate random string 0000-0000-0000-0000.
+        $orcid = implode('-', array_map(function ($value) {
+          return str_pad(rand(0, pow(10, $value)-1), $value, '0', STR_PAD_LEFT);
+        }, array_fill(0, 4, 4)));
+        // Some orcid's may have a last character of X.
+        $orcid = (rand(0, 10) === 0) ? substr($orcid, 0, -1) . 'X' : $orcid;
         return [
-          'value' => implode('-', $orcid) . substr(implode('', range(0, 9)) . 'X', rand(0, 10), 1),
+          'value' => $orcid,
         ];
         break;
       default:
