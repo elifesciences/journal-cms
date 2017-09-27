0b1a79112965c4929765f9a99d49cda5db0778ab
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-27 13:20:06 +0000

    Moving up fixture setup to the first functional test that is executed

--- src/modules/jcms_rest/tests/src/Functional/FixtureBasedTestCase.php
+++ src/modules/jcms_rest/tests/src/Functional/FixtureBasedTestCase.php
@@ -5,4 +5,25 @@ namespace Drupal\Tests\jcms_rest\Functional;
 use Drupal\Tests\UnitTestCase;
 
 abstract class FixtureBasedTestCase extends UnitTestCase {
+
+  protected static $contentGenerated = FALSE;
+
+  public static function setUpBeforeClass()
+  {
+    parent::setUpBeforeClass();
+    // Generate content once.
+    if (!self::$contentGenerated) {
+      self::$contentGenerated = TRUE;
+      $projectRoot = realpath(__DIR__ . '/../../../../../..');
+      $script = $projectRoot . '/scripts/generate_content.sh';
+      if (!file_exists($script)) {
+        throw new RuntimeException("File $script does not exist");
+      }
+      $logFile = '/tmp/generate_content.log';
+      exec("$script >$logFile 2>&1", $output, $exitCode);
+      if ($exitCode != 0) {
+        throw new RuntimeException("$script failed. Check log file $logFile");
+      }
+    }
+  }
 }
--- src/modules/jcms_rest/tests/src/Functional/RamlSchemaValidationTest.php
+++ src/modules/jcms_rest/tests/src/Functional/RamlSchemaValidationTest.php
@@ -30,27 +30,6 @@ class RamlSchemaValidationTest extends FixtureBasedTestCase {
    */
   protected $validator;
 
-  protected static $contentGenerated = FALSE;
-
-  public static function setUpBeforeClass()
-  {
-    parent::setUpBeforeClass();
-    // Generate content once.
-    if (!self::$contentGenerated) {
-      self::$contentGenerated = TRUE;
-      $projectRoot = realpath(__DIR__ . '/../../../../../..');
-      $script = $projectRoot . '/scripts/generate_content.sh';
-      if (!file_exists($script)) {
-        throw new RuntimeException("File $script does not exist");
-      }
-      $logFile = '/tmp/generate_content.log';
-      exec("$script >$logFile 2>&1", $output, $exitCode);
-      if ($exitCode != 0) {
-        throw new RuntimeException("$script failed. Check log file $logFile");
-      }
-    }
-  }
-
   function setUp() {
     parent::setUp();
     $this->validator = new FakeHttpsMessageValidator(
