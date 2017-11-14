f79c66d642287d3bd34749907f38cf8e617ebdc6
nlisgo <nathan@aeopus.com>
2017-11-14 15:51:26 +0000

    Improve explanation

--- src/modules/jcms_rest/src/Plugin/Field/FieldTypeExtend/EntityReferenceItem.php
+++ src/modules/jcms_rest/src/Plugin/Field/FieldTypeExtend/EntityReferenceItem.php
@@ -19,7 +19,24 @@ class EntityReferenceItem extends EntityReferenceItemExtend {
       // Random values must be unique.
       case 'field_subjects':
         static $unique = [];
-        // 25 subject terms are generated and the maximum number associated with content is 3.
+        /**
+         * As field_subjects has a cardinality of unlimited this method may be
+         * called between 1 and 3 times for each content item generated.
+         *
+         * @see: \Drupal\devel_generate\DevelGenerateBase::populateFields
+         * if ($cardinality == FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED) {
+         *   // Just an arbitrary number for 'unlimited'
+         *   $max = rand(1, 3);
+         * }
+         *
+         * In order to avoid duplicates, we should ensure that 25 or more
+         * subject terms are available (./scripts/generate_content.sh).
+         *
+         * When this method is called it could be the 1st, 2nd or 3rd value for
+         * the content.
+         * To ensure the 3rd item is unique for the content we must ensure it
+         * doesn't match the previous 2 values retrieved.
+         */
         if (count($unique) === 10) {
           $unique = array_slice(array_values($unique), -2);
         }
