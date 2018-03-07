859a79575b7be0cd2332f1c2e84e83159f63a80c
nlisgo <nathan@aeopus.com>
2018-03-07 00:11:16 +0000

    coder_sniffer

--- project_tests.sh
+++ project_tests.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+echo "coder_sniffer"
+phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer
+phpcs --standard=Drupal ./src
+
+echo "PHPUnit tests"
 export SIMPLETEST_DB=sqlite://localhost/sites/default/files/.ht.sqlite
 
-# no tests executed yet, but ready for the future
 vendor/bin/phpunit --log-junit build/phpunit.xml
