1c127731b201ff98bcd4bb1f57f325ee61efc3e2
nlisgo <nathan@aeopus.com>
2018-03-07 13:38:16 +0000

    Add squizlabs/php_codesniffer as a dependency

--- project_tests.sh
+++ project_tests.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "coder_sniffer"
-phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer
-phpcs --standard=Drupal ./src
+vendor/bin/phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer
+vendor/bin/phpcs --standard=Drupal ./src
 
 echo "PHPUnit tests"
 export SIMPLETEST_DB=sqlite://localhost/sites/default/files/.ht.sqlite
