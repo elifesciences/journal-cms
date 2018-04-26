372f4632aae63cfe8ef498c0413aa435919a7149
nlisgo <nathan@aeopus.com>
2018-04-26 17:35:54 +0100

    Pass all arguments

--- scripts/sanitized-db-dump.sh
+++ scripts/sanitized-db-dump.sh
@@ -1,39 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-COMPRESS=""
+DEFAULT_STRUCTURED_TABLES_LIST=" --structure-tables-list='user__roles,user__user_picture,users,users_data,users_field_data'"
 
-#########################
-# The command line help #
-#########################
-display_help() {
-    echo "Usage: $(basename "$0") [-h] [--gzip]"
-    echo
-    echo " --gzip Compress the dump using the gzip program which must be in your \$PATH."
-    exit 1
-}
-
-################################
-# Check if parameters options  #
-# are given on the commandline #
-################################
-while true;
-do
-    case "$1" in
-      -h | --help)
-          display_help
-          exit 0
-          ;;
-      --gzip)
-          COMPRESS="$1 "
-          shift 1
-          ;;
-      *)  # No more options
-          break
-          ;;
-    esac
-done
+if [[ ${*} =~ \-\-structure\-tables\-list ]];
+then
+    DEFAULT_STRUCTURED_TABLES_LIST=""
+fi
 
 cd "$(dirname $0)/../web"
 
-../vendor/bin/drush sql-dump $COMPRESS--structure-tables-list="user__roles,user__user_picture,users,users_data,users_field_data"
+../vendor/bin/drush sql-dump $* $DEFAULT_STRUCTURED_TABLES_LIST
