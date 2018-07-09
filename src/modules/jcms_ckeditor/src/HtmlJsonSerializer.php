9428a5d4d70f69b91f95eb38fb9e5b4ef2b31c3b
Mike Diplock <mikediplock@f2s.com>
2018-07-09 11:20:20 +0100

    Add jcms_ckeditor module to provide inline ckeditor editing.

---
+++ src/modules/jcms_ckeditor/src/HtmlJsonSerializer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\jcms_ckeditor;
+
+use League\CommonMark\DocParser;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class HtmlJsonSerializer implements NormalizerInterface
+{
+
+    private $htmlMardownNormalizer;
+    private $markdownJsonNormalizer;
+    private $docParser;
+
+    public function __construct(HtmlMarkdownSerializer $htmlMardownNormalizer, MarkdownJsonSerializer $markdownJsonNormalizer, DocParser $docParser)
+    {
+        $this->htmlMardownNormalizer = $htmlMardownNormalizer;
+        $this->markdownJsonNormalizer = $markdownJsonNormalizer;
+        $this->docParser = $docParser;
+    }
+
+    /**
+     * @param string $object
+     */
+    public function normalize($object, $format = null, array $context = []) : array
+    {
+        $markdown = $this->htmlMardownNormalizer->normalize($object, $format, $context);
+        return $this->markdownJsonNormalizer->normalize($this->docParser->parse($markdown), $format, $context);
+    }
+
+    public function supportsNormalization($data, $format = null) : bool
+    {
+        return is_string($data);
+    }
+
+}
