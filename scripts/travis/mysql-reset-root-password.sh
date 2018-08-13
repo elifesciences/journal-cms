f701b43ba95fb53c623fbc37eff838ddd7d570e8
nlisgo <nathan@aeopus.com>
2018-08-13 14:57:23 +0100

    Drop travis support

--- scripts/travis/mysql-reset-root-password.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -e
-sudo service mysql stop
-sudo  mysqld_safe --skip-grant-tables --pid-file=/var/lib/mysql/mysqld_safe.pid &
-sleep 4
-sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
-sudo kill -9 `sudo cat /var/lib/mysql/mysqld_safe.pid`
-sleep 4
