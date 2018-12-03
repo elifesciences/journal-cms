91980e4fcdc15b729b9459323c2529326ffe4d26
Giorgio Sironi <g.sironi@elifesciences.org>
2018-12-03 16:20:42 +0000

    Add script to restore backups

---
+++ scripts/restore-backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+TMP="${TMP:-/ext/tmp}"
+filesArchive="$(ls "$TMP/*-archive-*.tar.gz")"
+databaseArchive="$(ls "$TMP/*-elife_2_0-mysql.gz")"
+
+echo "Extracting $filesArchive"
+tar -xzf "$filesArchive" -C /ext/tmp
+
+echo "Restoring $filesArchive"
+rm -rf /srv/journal-cms/web/sites/default/files
+mv /ext/tmp/srv/journal-cms/web/sites/default/files web/sites/default/
+sudo chown -R www-data:www-data /srv/journal-cms/web/sites/default/files
+rm -rf /ext/tmp/srv
+
+echo "Restoring $databaseArchive"
+cd web
+../vendor/bin/drush sql-drop -y
+zcat "$databaseArchive" | ../vendor/bin/drush sql-cli
+rm "$databaseArchive"
