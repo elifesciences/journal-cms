deebcd0869cceea3ec98381c8afa1a96e50f72bb
nlisgo <nathan@aeopus.com>
2018-12-04 12:28:04 +0000

    Tidy up and all project root to be overridden

--- scripts/restore-backup.sh
+++ scripts/restore-backup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
+# Usage: TMP=FOLDER ROOT=FOLDER scripts/restore-backup.sh
+# Example: TMP=/ext/tmp ROOT=/srv/journal-cms scripts/restore-backup.sh
 
+# arguments and environment variables to influence behavior
 TMP="${TMP:-/ext/tmp}"
+ROOT="${ROOT:-/srv/journal-cms}"
+
 # cannot use double quotes to allow bash to resolve * wildcards
 # shellcheck disable=SC2086
 filesArchive=$(find $TMP/*-archive-*.tar.gz)
@@ -9,17 +14,17 @@ filesArchive=$(find $TMP/*-archive-*.tar.gz)
 databaseArchive=$(find $TMP/*-elife_2_0-mysql.gz)
 
 echo "Extracting $filesArchive"
-tar -xzf "$filesArchive" -C /ext/tmp
+tar -xzf "$filesArchive" -C $TMP
 
 echo "Restoring $filesArchive"
-rm -rf /srv/journal-cms/web/sites/default/files
-mv /ext/tmp/srv/journal-cms/web/sites/default/files web/sites/default/
-sudo chown -R www-data:www-data /srv/journal-cms/web/sites/default/files
-rm -rf /ext/tmp/srv
+rm -rf $ROOT/web/sites/default/files
+mv $TMP/srv/journal-cms/web/sites/default/files $ROOT/web/sites/default/
+sudo chown -R www-data:www-data $ROOT/web/sites/default/files
+rm -rf $TMP/srv
 rm -f "$filesArchive"
 
 echo "Restoring $databaseArchive"
-cd web
+cd $ROOT/web
 ../vendor/bin/drush sql-drop -y
 zcat "$databaseArchive" | ../vendor/bin/drush sql-cli
 rm "$databaseArchive"
