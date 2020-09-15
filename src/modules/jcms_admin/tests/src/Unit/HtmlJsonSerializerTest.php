cfe794b6b4b5f758d12d3e7a09dc91d117d35c35
NuclearRedeye <j.summerfield@elifesciences.org>
2020-09-15 18:16:12 +0100

    chore: add new test case

--- src/modules/jcms_admin/tests/src/Unit/HtmlJsonSerializerTest.php
+++ src/modules/jcms_admin/tests/src/Unit/HtmlJsonSerializerTest.php
@@ -1125,6 +1125,46 @@ class HtmlJsonSerializerTest extends UnitTestCase {
         [],
         '<p><placeholder>Type something here ...</placeholder></p>',
       ],
+      'bulleted lists' => [
+        [
+          [
+            'type' => 'paragraph',
+            'text' => 'Some Title',
+          ],
+          [
+            'type' => 'list',
+            'prefix' => 'bullet',
+            'items' => [
+              'Item 1',
+              'Item 2',
+            ],
+          ],
+          [
+            'type' => 'paragraph',
+            'text' => 'Another Title',
+          ],
+          [
+            'type' => 'list',
+            'prefix' => 'bullet',
+            'items' => [
+              'Item 1',
+              'Item 2',
+            ],
+          ],
+        ],
+        $this->lines([
+          '<p>Some Title</p>',
+          '<ul>',
+          '<li>Item 1</li>',
+          '<li>Item 2</li>',
+          '</ul>',
+          '<p>Another Title</p>',
+          '<ul>',
+          '<li>Item 1</li>',
+          '<li>Item 1</li>',
+          '</ul>'
+        ]),
+      ],
     ];
   }
 
