5a3eca3390f1c00ff11b7d1bf054eafc3f53c0a3
Paul Martin <paul_martin_84@hotmail.com>
2022-10-08 11:34:10 +0100

    Fix issues with local environment
    Ensure PHP 7.4 and Composer 1 in Drupal VM

--- scripts/pre-provision.sh
+++ scripts/pre-provision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+update-alternatives --set php /usr/bin/php7.4
+composer self-update --1
 if [[ -e /vagrant/web/sites/default  ]]; then
     chmod 755 /vagrant/web/sites/default /vagrant/web/sites/default/settings.php
 fi
