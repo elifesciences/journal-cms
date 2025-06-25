fa07d78272d0d9e1328090d076a565073229d9c6
Nathan Lisgo <nathan@lisgo.co.uk>
2025-06-25 12:26:24 +0100

    Remove article metrics processing logic and related commands
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1258

--- src/modules/jcms_notifications/src/Commands/JcmsNotificationsCommands.php
+++ src/modules/jcms_notifications/src/Commands/JcmsNotificationsCommands.php
@@ -122,7 +122,6 @@ class JcmsNotificationsCommands extends DrushCommands {
     $logger = \Drupal::logger('jcms_message_import');
     $queue_service = \Drupal::service('jcms_notifications.queue_service');
     $article_service = \Drupal::service('jcms_article.fetch_article_versions');
-    $metrics_service = \Drupal::service('jcms_article.fetch_article_metrics');
     $digest_service = \Drupal::service('jcms_digest.fetch_digest');
     $reviewed_preprint_service = \Drupal::service('jcms_article.fetch_reviewed_preprint');
     $article_crud_service = \Drupal::service('jcms_article.article_crud');
@@ -141,10 +140,8 @@ class JcmsNotificationsCommands extends DrushCommands {
         $logger->info('Received message', ['message' => $message->getMessage()]);
         $id = $message->getId();
 
-        // Temporary tolerance in id while articles and metrics id are
-        // inconsistent.
-        if (strlen($id) < 5 &&
-          in_array($message->getType(), ['article', 'metrics'])) {
+        // Temporary tolerance in id while article id is inconsistent.
+        if (strlen($id) < 5 && $message->getType() === 'article') {
           $id = str_pad($id, 5, '0', STR_PAD_LEFT);
           $logger->warning('Id for type is too short. Changing before to after.', [
             'type' => $message->getType(),
@@ -175,28 +172,7 @@ class JcmsNotificationsCommands extends DrushCommands {
               break;
 
             case 'metrics':
-              // Process article views-downloads metrics sqs items.
-              $body = $message->getMessage();
-              if ($body['contentType'] == 'article' && $body['metric'] == 'views-downloads') {
-                $nid = $article_crud_service->getNodeIdByArticleId($id);
-                if ($nid === 0) {
-                  $articleVersions = $article_service->getArticleVersions($id);
-                  $node = $article_crud_service->crudArticle($articleVersions);
-                }
-                else {
-                  $node = Node::load($nid);
-                }
-
-                $metrics = $metrics_service->getArticleMetrics($id);
-                if ((int) $node->get('field_page_views')->getString() != $metrics->getPageViews()) {
-                  $logger->info('Adjusted metrics', [
-                    'id' => $id,
-                    'metrics' => $metrics->getPageViews(),
-                  ]);
-                  $node->set('field_page_views', $metrics->getPageViews());
-                  $node->save();
-                }
-              }
+              $logger->info('Bypass metrics queue item', ['message_id' => $message->getMessageId()]);
               break;
           }
           $logger->info('Processed message', ['message_id' => $message->getMessageId()]);
@@ -425,76 +401,6 @@ class JcmsNotificationsCommands extends DrushCommands {
       $time = round($time_end - $time_start, 0);
       $this->output()->writeln(dt('Processed !count digests in !minutes minutes !seconds seconds.', [
         '!count' => count($digests),
-        '!minutes' => floor($time / 60),
-        '!seconds' => round($time % 60),
-      ]));
-    }
-  }
-
-  /**
-   * Imports all metrics for articles in journal-cms.
-   *
-   * @param array $options
-   *   Array of options whose values come from cli, aliases, config, etc.
-   *
-   * @option limit
-   *   Limit on the number of items to process in each import.
-   * @option skip-updates
-   *   Do not attempt to update articles that have a metric value already.
-   * @usage drush article-metrics-import-all
-   *   Import all article metrics in journal-cms and return a message when
-   * finished.
-   * @usage drush article-metrics-import-all --limit=500
-   *   Import first 500 article metrics in journal-cms and return a message
-   * when finished.
-   * @usage drush article-metrics-import-all --skip-updates
-   *   Import all article metrics in journal-cms, but skip over articles that
-   * we have a metric for already, and return a message when finished.
-   * @validate-module-enabled jcms_notifications
-   *
-   * @command article:metrics-import-all
-   * @aliases amia,article-metrics-import-all
-   */
-  public function articleMetricsImportAll(array $options = [
-    'limit' => NULL,
-    'skip-updates' => NULL,
-  ]) {
-    $metrics_service = \Drupal::service('jcms_article.fetch_article_metrics');
-    $this->output()->writeln(dt('Fetching article metrics. This may take a few minutes.'));
-    $limit = $options['limit'] ? (int) $options['limit'] : NULL;
-
-    $query = \Drupal::entityQuery('node')
-      ->accessCheck(TRUE)
-      ->condition('type', 'article');
-    if (!empty($limit)) {
-      $query->range(0, $limit);
-    }
-    if ($options['skip-updates']) {
-      $query->condition('field_page_views.value', 0);
-    }
-    $nids = $query->execute();
-    /** @var \Drupal\node\Entity\Node[] $nodes */
-    $nodes = Node::loadMultiple($nids);
-    $this->output()->writeln(dt('Received !count article metrics to process.', ['!count' => count($nids)]));
-    if ($nodes) {
-      $time_start = microtime(TRUE);
-      $num = 0;
-      foreach ($nodes as $nid => $node) {
-        $articleMetrics = $metrics_service->getArticleMetrics($node->label());
-        if ((int) $node->get('field_page_views')->getString() != $articleMetrics->getPageViews()) {
-          $node->set('field_page_views', $articleMetrics->getPageViews());
-          $node->save();
-        }
-        $this->output()->writeln(dt('Processed article metrics for !article_id (!num of !count)', [
-          '!article_id' => $node->label(),
-          '!num' => ++$num,
-          '!count' => count($nids),
-        ]));
-      }
-      $time_end = microtime(TRUE);
-      $time = round($time_end - $time_start, 0);
-      $this->output()->writeln(dt('Processed !count article metrics in !minutes minutes !seconds seconds.', [
-        '!count' => count($nids),
         '!minutes' => floor($time / 60),
         '!seconds' => round($time % 60),
       ]));
