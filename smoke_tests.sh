ded4604257ade506191105d574a71dd454f177b5
Scott Aubrey <scott@aubrey.org.uk>
2026-03-10 09:23:57 +0000

    attempt to fail tests over https

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -5,28 +5,28 @@ local_hostname=$(hostname)
 hostname=${1:-$local_hostname}
 
 echo "Ping"
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/ping") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/ping") == 200 ]
 
 echo "Homepage"
-[ $(curl --write-out %{http_code} --silent --output /dev/null "$hostname") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://$hostname") == 200 ]
 
 echo "APIs"
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/annual-reports") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/blog-articles") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/collections") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/community") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/covers") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/events") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/interviews") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/job-adverts") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/labs-posts") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/people") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/people?type=leadership") == 200 ] # Deprecated
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/people?type\[\]=leadership") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/podcast-episodes") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/press-packages") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/promotional-collections") == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null "${hostname}/subjects") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/annual-reports") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/blog-articles") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/collections") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/community") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/covers") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/events") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/interviews") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/job-adverts") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/labs-posts") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/people") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/people?type=leadership") == 200 ] # Deprecated
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/people?type\[\]=leadership") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/podcast-episodes") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/press-packages") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/promotional-collections") == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null "https://${hostname}/subjects") == 200 ]
 
 echo "Redis"
 php -r '$redis = new \Redis(); $redis->connect($argv[1], 6379);' "$hostname"
